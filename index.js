a02fcb059f97b741beb1ec9a75e96923424bd027
alefwhite <alefwhite@hotmail.com>
2019-10-19 03:34:29 -0300

    "Incluindo o bootstrap no projeto"

--- index.js
+++ index.js
@@ -7,23 +7,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    let nome = "Victor Lima";
-    let lang = "Javascript";
-    let produtos = [
-        {nome : "Doritos", preco : 5.00},
-        {nome : "Cheetos", preco : 3.00},
-        {nome : "Nescau", preco : 6.00},
-        {nome : "Fanta Laranja", preco : 5.00},
-        {nome : "Trakinas", preco : 2.00},
-        {nome : "Kit kat", preco : 1.50}
-        
-    ];
-
+   
     res.render("index", {
-        nome,
-        lang,
-        msg : false,
-        produtos
+      
     });    
 });
 
