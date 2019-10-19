f16d088d19ae9d4b70644931072ba535a382a0a9
alefwhite <alefwhite@hotmail.com>
2019-10-19 03:21:40 -0300

    teste

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3333;
+
+// Estou dizendo para o Express usar o ejs como View engine para desenhar meu html
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.send("Testando");
+    let nome = "Victor Lima";
+    let lang = "Javascript";
+    let produtos = [
+        {nome : "Doritos", preco : 5.00},
+        {nome : "Cheetos", preco : 3.00},
+        {nome : "Nescau", preco : 6.00},
+        {nome : "Fanta Laranja", preco : 5.00},
+        {nome : "Trakinas", preco : 2.00},
+        {nome : "Kit kat", preco : 1.50}
+        
+    ];
+
+    res.render("index", {
+        nome,
+        lang,
+        msg : false,
+        produtos
+    });    
 });
 
 // yarn dev 
-app.listen(app, function() {
-    console.log(`Servidor rodando na porta: ${port}`);
+app.listen(port, function() {
+    console.log(`Servidor rodando na porta: ${port}.`);
 });
